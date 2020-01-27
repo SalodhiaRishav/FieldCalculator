f700002fac53da4825705c38743a1d41fe0c60a9
Rishav Salodhia <rishav.salodhia@nagarro.com>
2020-01-27 14:07:57 +0530

    added calculation in infield Calculator

--- src/components/CalculatorInput.vue
+++ src/components/CalculatorInput.vue
@@ -1,7 +1,10 @@
 <template>
 <div>
     <div  v-bind:class="{ someBlur: blurInput }" class="input-container" >
-    <input class="input-field" type="text" v-model="inputFieldValue" :disabled="showCalculator">
+    <input class="input-field" type="text" v-model="inputFieldValue"
+     :class="{ 'redBorderOnFocus': !isValidExpression, 'greenBorderOnFocus': isValidExpression }"
+     :disabled="showCalculator"
+     @keyup ="onEnterPressed">
     <i class="fa fa-calculator icon" @click="onClickCalculatorIcon"></i>
   </div>
   <br>
@@ -18,12 +21,66 @@ export default {
     },
     data(){
         return{
+            validInputString:"1234567890.%*-+/=",
+            operatorString:".%*-+/",
             blurInput:false,
             inputFieldValue:"",
-            showCalculator:false
+            showCalculator:false,
+            isValidExpression:true
+        }
+    },
+    watch: {
+        inputFieldValue(val) {
+            let keyValue = '';
+            if(val.length > 0)
+            {
+                keyValue = val[val.length-1];
+            }
+            if (this.validInputString.indexOf(keyValue) === -1)
+            {
+                this.inputFieldValue =	this.inputFieldValue.slice(0, -1);
+            }
+            if(this.operatorString.indexOf(keyValue) > -1)
+                {
+                    let secondLastCharacter = this.inputFieldValue[this.inputFieldValue.length-2];
+                    if(this.operatorString.indexOf(secondLastCharacter) > -1)
+                    {
+                        this.inputFieldValue =	this.inputFieldValue.slice(0, -1);
+                    }
+            }
+
+            if( keyValue === "=" && this.inputFieldValue.length !== 1)
+            {
+                 this.inputFieldValue =	this.inputFieldValue.slice(0, -1);
+                 
+                 if(this.inputFieldValue[0] !== "=")
+                 {
+                     this.inputFieldValue="="+this.inputFieldValue;
+                 }
+                 else
+                 {
+                    let expressionToEvaluate = this.inputFieldValue.substring(1);
+                    this.inputFieldValue="="+this.$mathjs.evaluate(expressionToEvaluate).toString();
+                 }
+   
+            }
         }
     },
     methods:{
+        onEnterPressed(e)
+        {
+            if(e.keyCode === 13)
+            {
+                let finalResult = this.inputFieldValue.substring(1);
+                if(isNaN(finalResult)){
+                    return;
+                }
+                if(this.inputFieldValue[0] ==="=")
+                {
+                  this.inputFieldValue = finalResult;
+                }
+            }
+        },
         onCalcEnterPressed(e){
             this.inputFieldValue = e.result;
             this.blurInput=false;
@@ -33,9 +90,10 @@ export default {
         {
             this.blurInput=!this.showCalculator;
             this.showCalculator= !this.showCalculator;
-        }
+        },
+       
     }
-    
+
 }
 </script>
 
@@ -54,6 +112,14 @@ filter: blur(1px);
   width: 320px;
 }
 
+.greenBorderOnFocus{
+border: 1px solid green;
+}
+
+.redBorderOnFocus{
+border: 1px solid red;
+}
+
 .icon {
   padding: 10px;
   background: dodgerblue;
@@ -69,7 +135,7 @@ filter: blur(1px);
   outline: none;
 }
 
-.input-field:focus {
+/* .input-field:focus {
   border: 1px solid dodgerblue;
-}
+} */
 </style>
